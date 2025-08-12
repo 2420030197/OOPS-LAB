69e9ab7515dbcc2441d74e07a12ce876b1ecf3fc
2420030197 <2420030197@klh.edu.in>
2025-08-12 11:27:47 +0530

    Add files via upload

---
+++ Lab3/src/prgs/Displayname.java
@@ -0,0 +1,10 @@
+package prgs;
+
+public class Displayname {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Lab3/src/prgs/Vowels.java
@@ -0,0 +1,10 @@
+package prgs;
+
+public class Vowels {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
